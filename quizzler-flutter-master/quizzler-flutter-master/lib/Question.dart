296bbfbc0eb7d3495cbcf462cb37ffa5a89c6b9c
MohamedAdel7710 <mohomed.adel.26@gmail.com>
2020-08-26 17:42:56 +0200

    First commit

---
+++ quizzler-flutter-master/quizzler-flutter-master/lib/Question.dart
@@ -0,0 +1,9 @@
+class Question{
+    String questionText;
+    bool questionAnswer;
+
+    Question(String Q,bool A){
+      questionText = Q;
+      questionAnswer = A;
+    }
+}
---
+++ quizzler-flutter-master/quizzler-flutter-master/lib/main.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'QuizBrain.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
+
+void main() => runApp(Quizzler());
+
+QuizBrain quizBrain = QuizBrain();
+
+class Quizzler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: QuizPage(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class QuizPage extends StatefulWidget {
+  @override
+  _QuizPageState createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  void checkAnswer(bool userAnswer) {
+    bool quesAnswer = quizBrain.getQuestionAnswer();
+    setState(() {
+    if (quizBrain.quizEnd()) {
+      Alert(
+        context: context,
+        title: 'Finished!',
+        desc: 'You\'ve reached the end of the quiz.',
+      ).show();
+      quizBrain.quizReset();
+      scoreKeeper=[];
+    }else{
+
+    
+      if (userAnswer == quesAnswer) {
+        scoreKeeper.add(Icon(Icons.check, color: Colors.green));
+      } else {
+        scoreKeeper.add(Icon(Icons.close, color: Colors.red));
+      }
+      quizBrain.nextQuestion();
+    
+    }
+    });
+  }
+
+  Alert a;
+
+  List<Icon> scoreKeeper = [];
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        Expanded(
+          flex: 5,
+          child: Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Center(
+              child: Text(
+                quizBrain.getQuestionText(),
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              textColor: Colors.white,
+              color: Colors.green,
+              child: Text(
+                'True',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20.0,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(true);
+              },
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: EdgeInsets.all(15.0),
+            child: FlatButton(
+              color: Colors.red,
+              child: Text(
+                'False',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                checkAnswer(false);
+              },
+            ),
+          ),
+        ),
+        Row(children: scoreKeeper),
+      ],
+    );
+  }
+}
